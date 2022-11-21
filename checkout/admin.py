fe0932e2c85381e5ae566bf7f2d6e761f4b16447
Colin Shaw <colin.shaw400@outlook.com>
2022-11-21 20:58:40 +0000

    Add columns to show up in order list

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 # Register your models here.
 
+#functionality take form course content
 # class for order admin to generate administrion side
-
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     # read only fields so they can not be update and change the order
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'grand_total',)
-    
+    # columns to show up in order list
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)   
