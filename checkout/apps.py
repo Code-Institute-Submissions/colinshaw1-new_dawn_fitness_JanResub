ec371af1f4938c45c0c365289966f3af849081a3
Colin Shaw <colin.shaw400@outlook.com>
2022-11-22 17:53:10 +0000

    add signals to the app.py file

--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # overwrite ready method and import the signals
+    def ready(self):
+        import checkout.signals
