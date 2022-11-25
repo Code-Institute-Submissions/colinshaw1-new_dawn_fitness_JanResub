78508b2e4d40191b9e6d05724f4252e4e9fd1e4f
Colin Shaw <colin.shaw400@outlook.com>
2022-11-25 15:26:31 +0000

    Add fix to checkour succes url and update models.py file spelling mistake

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # url to the check out funciton
     path('', views.checkout, name='checkout'),
-    path('checkout_sucess/<order_number>', views.checkout_success, name='checkout_sucess'),
+    path('checkout_sucess/<order_number>', views.checkout_success, name='checkout_success'),
 ]
