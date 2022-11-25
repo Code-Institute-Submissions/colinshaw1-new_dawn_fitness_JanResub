b1bd5153f1a7da747e2a8ee01a1874179160338f
Colin Shaw <colin.shaw400@outlook.com>
2022-11-25 22:32:52 +0000

    Add order history url to link view

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # url to link the proile view
     path('', views.profile, name='profile')
     # view for order history
-    path('order_history/<order_number>', views.profile, name='order_history')
+    path('order_history/<order_number>', views.order_history, name='order_history')
 ]
