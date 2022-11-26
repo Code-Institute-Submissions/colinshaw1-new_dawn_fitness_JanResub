94e3cdcfb8baac67b7591d9004e4a5d4647dc2b3
Colin Shaw <colin.shaw400@outlook.com>
2022-11-26 07:41:40 +0000

    Deployment

--- new_dawn/settings.py
+++ new_dawn/settings.py
@@ -120,7 +120,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'new_dawn.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
