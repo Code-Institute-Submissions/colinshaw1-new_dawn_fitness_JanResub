17713853771786d29cc5653000a45c581da37815
Colin Shaw <colin.shaw400@outlook.com>
2022-11-26 09:09:20 +0000

    Add secret key and debug enviornemnt for heroku

--- new_dawn/settings.py
+++ new_dawn/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l*_ap(xshw9nu3bj%_rl-pnh^gzq57=jb&a907fk6tgc9xnm^b'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['new-dawn-fitness.herokuapp.com', 'localhost']
 
