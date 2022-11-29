ebde03318d0ec9eb63cde26b00b11d167040d8d6
Colin Shaw <colin.shaw400@outlook.com>
2022-11-29 18:46:06 +0000

    Deployment

--- new_dawn/settings.py
+++ new_dawn/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '#i2dq2ycd2%0nd$tbnplp=0-v4ie%hjey^w@x7ppjpe6j8^dfh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
